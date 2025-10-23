59fce640782b70278c0d64782c5618be4426ea69
Eren Erdogan <erenerdogan12323@gmail.com>
2025-10-23 15:53:04 +0300

    added piece-square tables
    -added piece-square tables and updated the evalutaion function. now, ai is also calculating the position for the moves.

--- ai.py
+++ ai.py
@@ -1,53 +1,42 @@
 import engine
 import random
-
-piece_scores = {
-    "K": 0,
-    "Q": 9,
-    "R": 5,
-    "B": 3,
-    "N": 3,
-    "p": 1
-}
+import piece_square_tables as pst # pst pst pst meow
 
 SEARCH_DEPTH = 3 # can be set to 4 after pruning
 best_move_found = None
 
-def score_material(game_state):
+# def score_material(game_state):
+#     score = 0
+#     for r in range(8):
+#         for c in range(8):
+#             piece = game_state.board[r][c]
+#             if piece is not None:
+#                 if piece.color == 'w':
+#                     score += pst.piece_scores[piece.type]
+#                 elif piece.color == 'b':
+#                     score -= pst.piece_scores[piece.type]
+#     return score
+
+def evaluate_board(game_state):
     score = 0
     for r in range(8):
         for c in range(8):
             piece = game_state.board[r][c]
             if piece is not None:
+                # piece scores
                 if piece.color == 'w':
-                    score += piece_scores[piece.type]
+                    score += pst.piece_scores[piece.type]
                 elif piece.color == 'b':
-                    score -= piece_scores[piece.type]
+                    score -= pst.piece_scores[piece.type]
+                
+                # positional scores
+                table = pst.piece_position_scores[piece.type]
+                if piece.color == 'w':
+                    score += table[r][c] / 100.0
+                elif piece.color == 'b': # flip the table for black
+                    score -= table[7-r][c] / 100.0
     return score
 
-# def find_best_move(game_state):
-#     valid_moves = game_state.get_legal_moves()
-
-#     if not valid_moves:
-#         return None
-
-#     random.shuffle(valid_moves)
-#     best_move = None
-
-#     best_score = float('inf')
-
-#     for move in valid_moves:
-#         game_state.make_move(move)
-
-#         score = minimax(game_state, SEARCH_DEPTH - 1, True)
-
-#         game_state.undo_move()
-
-#         if score < best_score:
-#             best_score = score
-#             best_move = move
-
-#     return best_move
 
 def find_best_move(game_state):
     global best_move_found
@@ -62,7 +51,7 @@ def minimax_pruning(game_state, depth, alpha, beta, is_maximizing_player):
     global best_move_found
 
     if depth == 0:
-        return score_material(game_state)
+        return evaluate_board(game_state)
     
     valid_moves = game_state.get_legal_moves()
     random.shuffle(valid_moves)
@@ -107,46 +96,3 @@ def minimax_pruning(game_state, depth, alpha, beta, is_maximizing_player):
             if beta <= alpha:
                 break
         return best_score
-
-
-
-
-
-# def minimax(game_state, depth, is_maximizing_player):
-#     if depth == 0:
-#         return score_material(game_state)
-    
-#     valid_moves = game_state.get_legal_moves()
-
-#     if not valid_moves:
-#         if game_state.in_check(): # checkmate is best move
-#             return float('-inf') if is_maximizing_player else float('inf')
-#         else: # stalemate
-#             return 0
-        
-#     # recursive
-#     if is_maximizing_player: # white
-#         best_score = -float('inf')
-#         for move in valid_moves:
-#             game_state.make_move(move)
-#             score = minimax(game_state, depth - 1, False)
-#             game_state.undo_move()
-#             best_score = max(best_score, score)
-#         return best_score
-#     else: # black
-#         best_score = float('inf')
-#         for move in valid_moves:
-#             game_state.make_move(move)
-#             score = minimax(game_state, depth - 1, True)
-#             game_state.undo_move()
-#             best_score = min(best_score, score)
-#         return best_score
-
-# def find_random_move(game_state):
-#     valid_moves = game_state.get_legal_moves()
-
-#     if not valid_moves:
-#         return None
-    
-#     random_index = random.randint(0, len(valid_moves) - 1)
-#     return valid_moves[random_index]
